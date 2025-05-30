6091166027aa3d9d4acb3a706b4f14219e46f88a
eratchia <eratchias@gmail.com>
2025-05-30 08:53:11 +0200

    impl

--- blas-dmmmult.cpp
+++ blas-dmmmult.cpp
@@ -7,6 +7,18 @@
 #include <random>
 #include <chrono>
 
+void mat_mult(double* A, double* B, double* C, int Ax, int Ay, int Bx) {
+    for(int x = 0; x < Bx; x++) {
+        for(int y = 0; y < Ay; y++) {
+            double res = 0;
+            for(int i = 0; i < Ax; i++) {
+                res += A[y * Ax + i] * B[i * Bx + x];
+            }
+            C[y * Bx + x] = res;
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cerr << "invocation: " <<argv[0]<<" matrix_size " << std::endl;
@@ -19,6 +31,7 @@ int main(int argc, char* argv[]) {
     double* A = new double[n*n];
     double* B = new double[n*n];
     double* C = new double[n*n];
+    double* Cown = new double[n*n];
     // double* A = (double*) malloc(sizeof(double)*n*n);
     // double* B = (double*) malloc(sizeof(double)*n*n);
     // double* C = (double*) malloc(sizeof(double)*n*n);
@@ -28,6 +41,7 @@ int main(int argc, char* argv[]) {
             A[i*n + j] = doubleDist(rnd);
             B[i*n + j] = doubleDist(rnd);
             C[i*n + j] = 0;
+            Cown[i*n + j] = 0;
         }
     }
 
@@ -39,5 +53,12 @@ int main(int argc, char* argv[]) {
     auto finishTime = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed{finishTime - startTime};
     std::cout << "CBLAS elapsed time: "<< elapsed.count() << "[s]" << std::endl;
+
+    startTime = std::chrono::steady_clock::now();
+    // this segfaults for matrix size 5000 and more
+    mat_mult(A, B, Cown, n, n, n);
+    finishTime = std::chrono::steady_clock::now();
+    elapsed = finishTime - startTime;
+    std::cout << "Own elapsed time: "<< elapsed.count() << "[s]" << std::endl;
 }
 
